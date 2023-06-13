fca8e2f798222f5de5a5471fc10bce4c843be6d6
Sachith Shetty <132273464+SachShetty@users.noreply.github.com>
2023-06-13 14:35:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 import { process } from '/env'
 import { Configuration, OpenAIApi } from 'openai'
 
+const setupInputContainer = document.getElementById('setup-input-container')
+const IntroText = document.getElementById('intro-text')
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 })
 
 const openai = new OpenAIApi(configuration)
 
-const setupInputContainer = document.getElementById('setup-input-container')
-const IntroText = document.getElementById('intro-text')
-
 document.getElementById("send-btn").addEventListener("click", () => {
   const setupTextarea = document.getElementById('setup-textarea') 
   if (setupTextarea.value) {
     const userInput = setupTextarea.value
     setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
-    IntroText.innerText = `Processing your ideas`
+
+  fetchBotReply()
   fetchName(userInput)
   }
 }) 
+
+async function fetchBotReply(){
+  IntroText.innerText = "Your character is being created"
+}
+
 
 async function fetchName(outline){
   const response = await openai.createCompletion({
@@ -53,7 +59,8 @@ async function fetchSynopsis(outline, name){
 
 async function fetchImage(outline, name){
   const response = await openai.createImage({
-    prompt: `Create a character portrait for ${outline} with name ${name}. The portrait should have an appropriate background and artstyle. Keep in mind the genre, setting.`,
+    prompt: `Create a character portrait for ${outline} with name ${name}. The portrait should have an appropriate background and artstyle. 
+    Keep in mind the genre, setting. The image should contain no text`,
     n: 1,
     size : '256x256',
     response_format: 'b64_json'
