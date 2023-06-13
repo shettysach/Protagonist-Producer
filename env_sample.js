75a8095a279ee31872b18b83177f3ab545e7b153
Sachith Shetty <132273464+SachShetty@users.noreply.github.com>
2023-06-13 13:56:15 +0530

    Add files via upload

---
+++ env_sample.js
@@ -0,0 +1,11 @@
+//Paste your OpenAI API key in string format in inplace of "Your OpenAI API Key"
+//Then rename this file to 'env.js'
+
+export const process = {
+    env: {
+        OPENAI_API_KEY: "Your OpenAI API Key"
+    }
+}
+
+//How to get an OpenAI API key
+//https://tfthacker.medium.com/how-to-get-your-own-api-key-for-using-openai-chatgpt-in-obsidian-41b7dd71f8d3//
